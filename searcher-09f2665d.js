955a3abd1c955b4be7aa908ce68fd7cd2bbcc511
nixpt <nixpt@users.noreply.github.com>
2026-07-14 15:52:41 +0000

    deploy: 7259bad4609b4e26d24c8270388e2da15139c84c

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0cca918e.js',
+                path_to_root + 'searchindex-b7a090aa.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
